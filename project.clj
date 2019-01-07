34145544e64948ee8f2302f527c8e6e942e1aa5b
Henrik Mohr <luposlip@gmail.com>
2019-01-07 19:37:49 +0100

    Github URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject luposlip/json-schema "0.1.0"
   :description "Clojure library for validating via JSON Schema - Draft-07 compatible"
-  :url "https://github.com/luposlip"
+  :url "https://github.com/luposlip/json-schema"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repositories {"jitpack.io" {:url "https://jitpack.io"}}
