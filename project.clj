906e267fafa0e4510939b0666b5fb560a779d3bb
Henrik Mohr <luposlip@gmail.com>
2019-02-28 11:08:25 +0100

    Bumped for deployment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luposlip/json-schema "0.1.0"
+(defproject luposlip/json-schema "0.1.1"
   :description "Clojure library for validating via JSON Schema - Draft-07 compatible"
   :url "https://github.com/luposlip/json-schema"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
