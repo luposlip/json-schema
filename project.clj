90e393f58b789159d533e2ea06ebeeca1a327df5
Henrik Mohr <luposlip@gmail.com>
2019-04-29 04:13:34 +0200

    Now using Apache License, Version 2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject luposlip/json-schema "0.1.3"
   :description "Clojure library for validating via JSON Schema - Draft-07 compatible"
   :url "https://github.com/luposlip/json-schema"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :repositories {"jitpack.io" {:url "https://jitpack.io"}}
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :repositories {"jitpack" {:url "https://jitpack.io"}}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cheshire "5.8.1"]
                  [com.github.everit-org.json-schema/org.everit.json.schema "1.11.1"]]
