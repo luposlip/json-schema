ae47cadd98c50ce75a1dd4acd0159a2ffe356f5b
Henrik Mohr <luposlip@gmail.com>
2020-04-23 13:08:01 +0200

    Fix tests

--- test/json_schema/infer_test.clj
+++ test/json_schema/infer_test.clj
@@ -8,65 +8,65 @@
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:thing {:type :integer}}
-          :required [:thing]}
+          :properties {"thing" {:type :integer}}
+          :required ["thing"]}
          (t/infer-strict {:thing 1} {:schema-name "ent-1"})))
 
   (is (= {:$schema "http://json-schema.org/draft-07/schema#"
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:thing {:type :object
-                               :additionalProperties false
-                               :properties {:quantity {:type :integer}}
-                               :required [:quantity]}}
-          :required [:thing]}
+          :properties {"thing" {:type :object
+                                :additionalProperties false
+                                :properties {"quantity" {:type :integer}}
+                                :required ["quantity"]}}
+          :required ["thing"]}
          (t/infer-strict {:thing {:quantity 1}} {:schema-name "ent-1"})))
 
   (is (= {:$schema "http://json-schema.org/draft-07/schema#"
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:thing {:type :object
-                               :additionalProperties false
-                               :properties {:quantity {:type :number}}
-                               :required [:quantity]}}
-          :required [:thing]}
+          :properties {"thing" {:type :object
+                                :additionalProperties false
+                                :properties {"quantity" {:type :number}}
+                                :required ["quantity"]}}
+          :required ["thing"]}
          (t/infer-strict {:thing {:quantity 1.1}} {:schema-name "ent-1"})))
   
   (is (= {:$schema "http://json-schema.org/draft-07/schema#"
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:thing {:type :object
-                               :additionalProperties false
-                               :properties {:quantity {:type :string}}
-                               :required [:quantity]}}
-          :required [:thing]}
+          :properties {"thing" {:type :object
+                                :additionalProperties false
+                                :properties {"quantity" {:type :string}}
+                                :required ["quantity"]}}
+          :required ["thing"]}
          (t/infer-strict {:thing {:quantity "11.111,11"}} {:schema-name "ent-1"})))
 
   (is (= {:$schema "http://json-schema.org/draft-07/schema#"
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:things {:type :array
-                                :items {:type :object
-                                        :additionalProperties false
-                                        :properties {:quantity {:type :integer}}
-                                        :required [:quantity]}}}
-          :required [:things]}
+          :properties {"things" {:type :array
+                                 :items {:type :object
+                                         :additionalProperties false
+                                         :properties {"quantity" {:type :integer}}
+                                         :required ["quantity"]}}}
+          :required ["things"]}
          (t/infer-strict {:things [{:quantity 1} {:quantity 2}]} {:schema-name "ent-1"})))
 
   (is (= {:$schema "http://json-schema.org/draft-07/schema#"
           :title "ent-1" 
           :type :object
           :additionalProperties false
-          :properties {:thing {:type :object
-                               :additionalProperties false
-                               :properties {:quantities {:type :array
-                                                         :items {:type :number}}}
-                               :required [:quantities]}}
-          :required [:thing]}
+          :properties {"thing" {:type :object
+                                :additionalProperties false
+                                :properties {"quantities" {:type :array
+                                                           :items {:type :number}}}
+                                :required ["quantities"]}}
+          :required ["thing"]}
          (t/infer-strict {:thing {:quantities [1.3 2.2 3.1]}} {:schema-name "ent-1"}))))
 
 (deftest infer-array
@@ -95,10 +95,10 @@
           :items
           {:type :object,
            :additionalProperties false,
-           :properties {:quantity {:type :integer}},
-           :required [:quantity]}}
+           :properties {"quantity" {:type :integer}},
+           :required ["quantity"]}}
          (t/infer-strict [{:quantity 1}] {:schema-name "ent-1"}))))
 
 (deftest infer-strict->json
-  (is (= "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"ent-1\",\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"thing\":{\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"quantities\":{\"type\":\"array\",\"items\":{\"type\":\"number\"}}},\"required\":[\"quantities\"]}},\"required\":[\"thing\"]}"
+  (is (= "{\"$schema\":\"http://jsong-schema.org/draft-07/schema#\",\"title\":\"ent-1\",\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"thing\":{\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"quantities\":{\"type\":\"array\",\"items\":{\"type\":\"number\"}}},\"required\":[\"quantities\"]}},\"required\":[\"thing\"]}"
          (t/infer-strict->json {:thing {:quantities [1.3 2.2 3.1]}} {:schema-name "ent-1"}))))
