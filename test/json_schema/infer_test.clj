4165c339a9a228473107ae2551a14c6292a4ed35
Henrik Mohr <luposlip@gmail.com>
2020-04-23 13:38:15 +0200

    Add test that validate based on inferred schema

--- test/json_schema/infer_test.clj
+++ test/json_schema/infer_test.clj
@@ -1,5 +1,6 @@
 (ns json-schema.infer-test
   (:require [clojure.test :refer :all]
+            [json-schema.core :as v]
             [json-schema.infer :as t]))
 
 (deftest infer-map
@@ -102,3 +103,13 @@
 (deftest infer-strict->json
   (is (= "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"ent-1\",\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"thing\":{\"type\":\"object\",\"additionalProperties\":false,\"properties\":{\"quantities\":{\"type\":\"array\",\"items\":{\"type\":\"number\"}}},\"required\":[\"quantities\"]}},\"required\":[\"thing\"]}"
          (t/infer-strict->json {:thing {:quantities [1.3 2.2 3.1]}} {:schema-name "ent-1"}))))
+
+(deftest infer-and-validate
+  (let [data {:things [{:quantity 1} {:quantity 2}]
+              :yearly {2020 1
+                       2021 2
+                       2022 2.5
+                       2023 3
+                       2024 (/ 1 3)}}
+        schema (t/infer-strict data {:schema-name "ent-1"})]
+    (is (= data (v/validate schema data)))))
